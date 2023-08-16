652a3c9e64bbe5a1f46b2d0d0048cd36f2f2742c
汪智鹏 <1848993253@qq.com>
2023-08-16 11:06:02 +0800

    解析请求url

--- node-serve/app.js
+++ node-serve/app.js
@@ -1,1 +1,16 @@
-console.log('Hello Node!')
+const querystring = require('querystring');
+
+const serverHandle = (req, res) => {
+    console.log(req.url);
+    // 设置返回格式 JSON
+    res.setHeader('Content-type', 'application/json');
+
+    // 获取 path
+    const url = req.url;
+    req.path = url.split('?')[0];
+
+    // 解析 query
+    req.query = querystring.parse(url.split('?')[1]);
+}
+
+module.exports = serverHandle
