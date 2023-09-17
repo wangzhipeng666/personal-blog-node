51600e07a7aee034300122d593dd595c0c9550af
wangzhipeng <1848993253@qq.com>
2023-09-17 20:13:38 +0800

    koa2开发路由

--- blog-koa2/app.js
+++ blog-koa2/app.js
@@ -10,6 +10,8 @@ const redisStore = require('koa-redis')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
+const blog = require('./routes/blog')
+const user = require('./routes/user')
 
 const { REDIS_CONF } = require('./conf/db')
 
@@ -55,6 +57,8 @@ app.use(session({
 // routes
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
+app.use(blog.routes(), blog.allowedMethods())
+app.use(user.routes(), user.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
